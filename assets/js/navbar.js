6232bc14bf9603ff7a46e24b658de3d0695939de
jgcodes2020 <jackyguo18@hotmail.com>
2023-04-01 10:02:54 -0400

    add navbar resize script

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,11 +1,30 @@
-// VERY small script.
-// whenever you click on the "hamburger" icon it toggles the view, open and closed.
+let nav_inputs = $("nav > input.nav-toggle");
 
-$("input.nav-toggle").on("input", event => {
+// whenever you click on the "hamburger" icon it toggles the view, open and closed.
+nav_inputs.on("input", event => {
   let target = $(event.delegateTarget);
   let links = target.siblings("ul.nav-links");
   if (target.is(":checked"))
     links.css("height", links.prop("scrollHeight"));
   else
-  links.css("height", 0);
-})
+    links.css("height", 0);
+});
+
+// whenever the window is resized, it switches between mobile and desktop navbars.
+$(window).resize(event => {
+  nav_inputs.each(_ => {
+    let links = $(this).siblings("ul.nav-links");
+
+    if (window.matchMedia("screen and (min-width: 768px)").matches) {
+      links.css("height", links.prop("scrollHeight"));
+    }
+    else {
+      if ($(this).is(":checked")) {
+        links.css("height", links.prop("scrollHeight"));
+      }
+      else {
+        links.css("height", 0);
+      }
+    }
+  })
+});
