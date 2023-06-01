594869ba1ffcaa80be2962fa92241080ee06326b
jgcodes2020 <jackyguo18@hotmail.com>
2023-06-01 11:33:52 -0400

    improve navbar script

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,9 +1,9 @@
-let nav_inputs = $("nav > input.nav-toggle");
+let check = $("nav > input.nav-toggle");
+let links = $("nav > ul.nav-links")
 
 // whenever you click on the "hamburger" icon it toggles the view, open and closed.
-nav_inputs.on("input", event => {
+check.on("input", event => {
   let target = $(event.delegateTarget);
-  let links = target.siblings("ul.nav-links");
   if (target.is(":checked"))
     links.css("height", links.prop("scrollHeight"));
   else
@@ -12,19 +12,17 @@ nav_inputs.on("input", event => {
 
 // whenever the window is resized, it switches between mobile and desktop navbars.
 $(window).resize(event => {
-  nav_inputs.each(_ => {
-    let links = $(this).siblings("ul.nav-links");
-
-    if (window.matchMedia("screen and (min-width: 768px)").matches) {
+  
+  if (window.matchMedia("screen and (min-width: 768px)").matches) {
+    links.css("height", "initial");
+    check.prop("checked", false);
+  }
+  else {
+    if (check.is(":checked")) {
       links.css("height", links.prop("scrollHeight"));
     }
     else {
-      if ($(this).is(":checked")) {
-        links.css("height", links.prop("scrollHeight"));
-      }
-      else {
-        links.css("height", 0);
-      }
+      links.css("height", 0);
     }
-  })
+  }
 });
