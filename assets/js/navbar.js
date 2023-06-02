c510fe889388a5148b8b7cf27f9dc60f4698f424
jgcodes2020 <jackyguo18@hotmail.com>
2023-06-02 10:59:09 -0400

    finish card css

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,27 +2,20 @@ let check = $("nav > input.nav-toggle");
 let links = $("nav > ul.nav-links")
 
 // whenever you click on the "hamburger" icon it toggles the view, open and closed.
-check.on("input", event => {
-  let target = $(event.delegateTarget);
-  if (target.is(":checked"))
-    links.css("height", links.prop("scrollHeight"));
-  else
-    links.css("height", 0);
-});
+// check.on("input", event => {
+//   let target = $(event.delegateTarget);
+//   if (target.is(":checked"))
+//     links.css("max-height", 100000);
+//   else
+//     links.css("max-height", 0);
+// });
 
 // whenever the window is resized, it switches between mobile and desktop navbars.
-$(window).resize(event => {
-  
-  if (window.matchMedia("screen and (min-width: 768px)").matches) {
-    links.css("height", "initial");
-    check.prop("checked", false);
-  }
-  else {
-    if (check.is(":checked")) {
-      links.css("height", links.prop("scrollHeight"));
-    }
-    else {
-      links.css("height", 0);
-    }
-  }
-});
+// $(window).resize(event => {
+//   if (window.matchMedia("screen and (min-width: 768px)").matches) {
+//     links.css("max-height", "initial");
+//     check.prop("checked", false);
+//   }
+// });
+
+links.css("--data-nav-scrollheight", `${links.prop("scrollHeight")}px`)
